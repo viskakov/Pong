5a43f300cc5f8c82cfc2381dedc80b27c515a6d3
Vsevolod Iskakov <vsev0l0d.vsevolod@gmail.com>
2022-02-03 13:05:40 +0300

    Added TextMeshPro package, added burger button sprite, added menu button

--- Assets/Scripts/ColorPickerManager.cs
+++ Assets/Scripts/ColorPickerManager.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ColorPickerManager : MonoBehaviour
 {
     [SerializeField] private GameObject _colorPickerPanel;
+    [SerializeField] private Button _menuButton;
 
     private bool _windowState;
 
     private void Awake()
     {
         _windowState = _colorPickerPanel.activeSelf;
+        _menuButton.onClick.AddListener(SwitchPanel);
     }
 
     private void Update()
